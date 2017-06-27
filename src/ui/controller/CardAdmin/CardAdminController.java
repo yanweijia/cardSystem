578499c20219f6291b1d3c265c34cfd9f7743037
yanweijia <happyboyywj@163.com>
2017-06-28 02:37:38 +0800

    backup

--- src/ui/controller/CardAdmin/CardAdminController.java
+++ src/ui/controller/CardAdmin/CardAdminController.java
@@ -1,7 +1,27 @@
 package ui.controller.CardAdmin;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by FXBL on 6/28/2017.
  */
-public class CardAdminController {
+public class CardAdminController implements Initializable{
+
+
+
+
+
+
+    /**
+     * 初始化
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
